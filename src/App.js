0b97faaa79a2a527a03d4e8581b0b6f091da3d0d
Alejandro Jimenez <ajim4268@github.com>
2018-11-04 12:47:11 -0700

    setup & header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="appHeader">
+         <h1 className="headerTitle">Savings Calculator</h1>
+         <Input />
+        </div>
+        <Input placeholder= "total"/>
       </div>
     );
   }
 }
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeholder : "",
+    }
+  }
+  render() {
+    return (
+      <div>
+        <input type="number" placeholder={this.Setstate="Emergency"}  />
+      </div>
+    );
+  }
+}
+
+class 
 
 export default App;
---
+++ src/accounts.js
@@ -0,0 +1,5 @@
+[
+ "Emergency",
+  "Car",
+  "School",
+]
