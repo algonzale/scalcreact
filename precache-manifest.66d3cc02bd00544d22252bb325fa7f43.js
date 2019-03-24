5c65410d6d97acabea47cecef41e9d81d253292d
Alejandro Jimenez <ajim4268@github.com>
2019-03-23 20:32:03 -0600

    Updates

---
+++ precache-manifest.66d3cc02bd00544d22252bb325fa7f43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97b43123197073d18439",
+    "url": "/scalcreact/static/js/runtime~main.71da2c01.js"
+  },
+  {
+    "revision": "9f82483b84b40b4a32eb",
+    "url": "/scalcreact/static/js/main.8826e47d.chunk.js"
+  },
+  {
+    "revision": "ebfc9c3183c1adfb9ea3",
+    "url": "/scalcreact/static/js/2.f0259060.chunk.js"
+  },
+  {
+    "revision": "9f82483b84b40b4a32eb",
+    "url": "/scalcreact/static/css/main.e6e3a78a.chunk.css"
+  },
+  {
+    "revision": "c1649f91abaeab8dc1790027b0bfbb29",
+    "url": "/scalcreact/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/scalcreact/precache-manifest.66d3cc02bd00544d22252bb325fa7f43.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/scalcreact/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
